811d27ca4a1086457c1e90a0db3ad3c4a1325f3c
bomgara228 <92594974+bomgara228@users.noreply.github.com>
2021-12-09 20:51:08 +0300

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+void randm(int** mas, int n, int m) {
+	srand(time(0));
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			mas[i][j] = rand() % 81 - 40;
+		}
+	}
+}
+
+void darand(int** mas, int n, int m) {
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << setw(4) << mas[i][j];
+		}
+		cout << endl;
+	}
+}
+
+void aaa(int** mas, int n, int m) {
+	int b = 0;
+	int k;
+	cout << "enter row number\n";
+	cin >> k;
+	for (int p = 0; p < n; p++) {
+		cout << mas[p][k] << ' ';
+	}
+	for (int p = 0; p < n; p++) {
+		if (mas[p][k] > 0) {
+			b += 1;
+		}
+	}
+	cout << endl << b;
+}
